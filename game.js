4d9c85433d7f70d664d1cf140ad0ffec1a0ce4de
nukaboy <niklas.kelm@web.de>
2017-05-29 12:59:52 +0200

    fuck

--- game.js
+++ game.js
@@ -93,7 +93,7 @@ function increaseKMH() {
 }
 
 function setRoadFrame(pos) {
-	document.getElementById("road") = roadFrames[pos%13];
+	document.getElementById("road").src = roadFrames[pos%13].src;
 }
 
 function setBlockPos() {
